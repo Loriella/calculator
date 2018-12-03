bc95f7a8f09e6f6665a2f6460facd921c1b4fb15
Loriella <loris.orlova@gmail.com>
2018-12-03 22:31:13 +0300

    added functions to display the result, delete the last character, clear all

--- js.js
+++ js.js
@@ -1,6 +1,45 @@
 const visual = document.querySelector('.visual_display');
+let visualResult = '';
+
+function setVisualResult(value) {
+    if (typeof value === "number") {
+        visualResult = String(value);
+    } else if (typeof value === "string") {
+        visualResult += value;
+    } else {
+        visualResult = '';
+    }
+
+    if (visualResult.length > 8) {
+        visual.innerHTML = visualResult.slice(0, 8);
+    } else {
+        visual.innerHTML = visualResult || '0';
+    }
+}
+
+function actionBackspace() {
+    visualResult = visualResult.slice(0, -1);
+    visual.innerHTML = visualResult || '0';
+}
+
+/**
+ * добавление обработчиков на цифровые кнопки
+ * */
 document.querySelectorAll('.btn-number').forEach(function (element) {
     element.onclick = function () {
-        visual.innerHTML = element.dataset.number;
+        setVisualResult(element.dataset.number)
     }
 });
+/**
+ * кнопка "очистить всё"
+ */
+document.querySelector('.btn-clear').onclick = function () {
+    setVisualResult();
+};
+
+/**
+ * кнопка backspace
+ */
+document.querySelector('.btn-backspace').onclick = function () {
+    actionBackspace();
+};
